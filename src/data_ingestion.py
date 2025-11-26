b75487e2c582f7c4b13e1927640fa7c2b1410b04
rasmikrtp <rasmikr41@gmail.com>
2025-11-26 11:25:52 +0000

    Add DVC pipeline configuration, requirements.txt, params.yaml, and all source components

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -86,10 +86,10 @@ def save_data(train_data: pd.DataFrame, test_data: pd.DataFrame, data_path: str)
 
 def main():
     try:
-        params = load_params(params_path='params.yaml')
-        test_size = params['data_ingestion']['test_size']
-        # test_size = 0.2
-        data_path = 'https://raw.githubusercontent.com/vikashishere/Datasets/main/spam.csv'
+        # params = load_params(params_path='params.yaml')
+        # test_size = params['data_ingestion']['test_size']
+        test_size = 0.2
+        data_path = 'https://raw.githubusercontent.com/taanjit/YT-MLOPS-End_to_End-unerstanding-about-ML-pipeline-and-DVC/refs/heads/main/experiments/spam.csv'
         df = load_data(data_url=data_path)
         final_df = preprocess_data(df)
         train_data, test_data = train_test_split(final_df, test_size=test_size, random_state=2)
